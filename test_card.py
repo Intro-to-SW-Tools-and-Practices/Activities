8c45719e937da0740881ca6042e1707e01684426
Quyen <qhttran1028@gmail.com>
2022-07-09 14:11:36 -0500

    Bug #6: class name convention

--- test_card.py
+++ test_card.py
@@ -1,6 +1,6 @@
 import unittest
 from Card import Card
-class Testing_Card(unittest.TestCase):
+class Testing_card(unittest.TestCase):
     def test_to_String(self):
         card = Card(1,1)
         self.assertEqual(type(card.to_String()), type("string"))
