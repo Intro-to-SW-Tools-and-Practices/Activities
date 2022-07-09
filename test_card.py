5ffd99a1078d6c5ce8c1a91b6c81561805438ddb
TheActualNalyd <studjho@yahoo.com>
2022-07-09 15:14:56 -0500

    Changed to Pascal Case

--- test_card.py
+++ test_card.py
@@ -1,15 +1,15 @@
 import unittest
 from Card import Card
-class Testing_Card(unittest.TestCase):
-    def Test_To_String(self):
+class Testing_Card(unittest.PascalCase):
+    def test_to_string(self):
         card = Card(1,1)
         self.assertEqual(type(card.to_String()), type("string"))
 
-    def Test_Get_Value(self):
+    def test_get_value(self):
         card = Card(1,1)
         self.assertEqual(card.get_value(), '2')
 
-    def Test_Get_Value2(self):
+    def test_get_value2(self):
         card = Card(0,1)
         self.assertEqual(card.get_value(), 'Ace')
 
