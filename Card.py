59890457636a050d3a285b103401b73c92920ee0
Ian-T-Elkin <ian.t.elkin@gmail.com>
2022-07-09 15:08:19 -0500

    minor whitespace changes, documentation, unittest fix

--- Card.py
+++ Card.py
@@ -6,19 +6,22 @@ Created on Fri Jan 28 15:13:28 2022
 @author: Brian Sun
 """
 
+"""Class docstring for Card."""
 class Card:
     suits = ["spades","hearts","diamonds","clubs"]
-    
+
     values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
-    
+
     def __init__(self,v,s):
         assert v <= 12, "value index out of bounds"
         assert s <= 3, "suit index out of bounds"
         self.value = v
         self.suit = s
-        
+
+"""Function docstring for to_String."""
     def to_String(self):
         return self.values[self.value] + " of " + self.suits[self.suit]
 
+"""Function docstring for get_value."""
     def get_value(self):
         return self.values[self.value]
--- test_card.py
+++ test_card.py
@@ -7,10 +7,10 @@ class Testing_Card(unittest.TestCase):
 
     def test_get_value(self):
         card = Card(1,1)
-        self.assertEqual(card.get_value(), '1')
+        self.assertEqual(card.get_value(), '2')
 
     def test_get_value2(self):
-        card = Card(0,1)
+        card = Card(0,0)
         self.assertEqual(card.get_value(), '1')
 
 if __name__ == '__main__':
