73c3b3cea1f93aea898080260fb5e7575849f2e2
TheActualNalyd <studjho@yahoo.com>
2022-07-09 14:49:47 -0500

    Fixed bug in card value list

--- Card.py
+++ Card.py
@@ -8,11 +8,11 @@ Created on Fri Jan 28 15:13:28 2022
 class Card:
     suits = ["spades", "hearts", "diamonds","clubs"]
     
-    values = ["Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+    values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
     
     def __init__(self,v,s):
         assert v <= 13, "value index out of bounds"
-        assert s <= 3, "suit index out of bounds"
+        assert s <= 4, "suit index out of bounds"
         self.value = v
         self.suit = s
         
