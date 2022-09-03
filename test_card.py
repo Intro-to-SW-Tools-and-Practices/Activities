a16cd92b1f8ccae66ada58ca616ce92deda30f69
alan-leonn <alan.leon12@yahoo.com>
2022-09-03 15:16:07 -0500

    Fixed unit test for test_card

--- test_card.py
+++ test_card.py
@@ -7,7 +7,7 @@ class Testing_Card(unittest.TestCase):
 
     def test_get_value(self):
         card = Card(1,1)
-        self.assertEqual(card.get_value(), '1')
+        self.assertEqual(card.get_value(), '2')
 
     def test_get_value2(self):
         card = Card(0,1)
